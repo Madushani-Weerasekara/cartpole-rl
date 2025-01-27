774ba9f606eca26320abe0c1bd80227cfdb9cfde
Madushani-Weerasekara <mmweerasekara@gmail.com>
2025-01-27 15:31:29 +0530

    Descrete the state space

--- train_agent.py
+++ train_agent.py
@@ -12,6 +12,10 @@ epsilon_min = 0.01 # Minimum exploration probasbility
 epsilon_decay = 0.995 # Exploration decay rate
 episodes = 1000 # Number of training episodes
  
- 
+# 3. Discretize the state space
+state_bins = [10, 10, 10, 10] # Number of bins for each state variable (position, velocity, etc.)
+state_bounds = list(zip(env.observation_space.low, env.observation_space.high))
+state_bounds[1] = [-0.5, 0.5] # Clip velocity range
+state_bounds[3] = [-50, 50] # Clip angular velocity range
 
 
